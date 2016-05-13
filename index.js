4adb56533dc8a9405505c9ac6834c0a99d574451
Sebastian Gräßl <sebastian@validcode.me>
2016-05-13 21:10:20 +0200

    First prototype

---
+++ index.js
@@ -0,0 +1,29 @@
+var path    = require('path');
+var express = require('express');
+var app     = express();
+
+var Chart           = require('./lib/chart');
+var GitHubReactions = require('./lib/github-resource');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/chart/:user/:repo/:resource/:id', function (req, res) {
+  console.log(req.params.resource);
+  if (['issues'].includes(req.params.resource)) {
+    var url      = req.params.user + '/' + req.params.repo + '/' + req.params.resource + '/' + req.params.id;
+    var resource = new GitHubReactions(url);
+    var chart    = new Chart(resource.collectedReactions(), 200, 400);
+
+    res.type('png');
+    res.send(chart.png());
+  } else {
+    res.status(404);
+    res.send('Not found');
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Reactions charts listening on port 3000!');
+});
